8a922802edb4aa40cd7c707b4d458ee922662ded
Yousef Kotp <41492875+yousefkotp@users.noreply.github.com>
2022-01-31 00:11:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('headless')
 chrome_options.add_argument("disable-gpu")
-driver = webdriver.Chrome(executable_path= r'chromedriver.exe', options=chrome_options)
+s = Service('chromedriver.exe')
+driver = webdriver.Chrome(service=s, options=chrome_options)
 url='https://www.powerlanguage.co.uk/wordle/'
 driver.get(url)
 scriptArray="return Array.apply(0, new Array(localStorage.length)).map(function (o, i) { return localStorage.getItem(localStorage.key(i));})"
@@ -10,3 +12,4 @@ result = driver.execute_script(scriptArray)
 s = result
 ans = s[0][105]+s[0][106]+s[0][107]+s[0][108]+s[0][109]
 print(ans)
+input("Press any key to exit: ")
